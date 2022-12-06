68b71221d897ca0afe5c83827b577dd00b8bb61f
reflectstars <reflectstars@proton.me>
2022-12-06 16:02:20 +0000

    iser-target: Handle ADDR_CHANGE event for listener cm_id

--- chatbot.py
+++ chatbot.py
@@ -201,3 +201,32 @@ import json
 import random
 
 with open('datasets/intents.json') as json_data:
+    intents = json.load(json_data)
+
+# Load model to predict user result
+loadedIntentClassifier = load_model('saved_state/intent_model.h5')
+loaded_intent_CV = pk.load(open('saved_state/IntentCountVectorizer.sav', 'rb'))    
+
+USER_INTENT = ""
+
+while True:
+    user_query = input()
+    
+    query = re.sub('[^a-zA-Z]', ' ', user_query)
+
+    # Tokenize sentence
+    query = query.split(' ')
+
+    # Lemmatizing
+    ps = PorterStemmer()
+    tokenized_query = [ps.stem(word.lower()) for word in query]
+
+    # Recreate the sentence from tokens
+    processed_text = ' '.join(tokenized_query)
+    
+    # Transform the query using the CountVectorizer
+    processed_text = loaded_intent_CV.transform([processed_text]).toarray()
+
+    # Make the prediction
+    predicted_Intent = loadedIntentClassifier.predict(processed_text)
+#     print(predicted_Intent)
